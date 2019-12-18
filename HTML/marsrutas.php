34771c628e831992f782b11033f9c6fd9a6d7134
Paulius Adamonis <paulius.adamonis@ktu.edu>
2019-12-19 01:13:10 +0200

    Maršruto pasirinkimas

--- HTML/marsrutas.php
+++ HTML/marsrutas.php
@@ -5,11 +5,11 @@
 		<div class="container">
 			<div class="row">
 				<div class="col">
-					<h3>Įgulos pasirinkimas</h3>
+					<h3>Maršruto pasirinkimas</h3>
 				</div>
 				<div class="col text-right">
-					<a class="btn btn-primary" href="interiorModules.php">Grįžti į raketos vidaus modeliavimą</a>
-					<a class="btn btn-success" href="marsrutas.php">Sudaryti maršrutą</a>
+                <a class="btn btn-primary" href="igula.php">Grįžti į įgulos pasirinkimą</a>
+					<a class="btn btn-success" href="location.php">Pasirinkti pakilimo vietą</a>
 				</div>
 			</div>
 			<hr>
@@ -17,29 +17,29 @@
 				<div class="row w-100">
 					<div class="col">
 						<div class="card">
-							<div class="card-title text-center">Narys nr.1</div>
-							<div class="card-body">nario foto</div>
+							<div class="card-title text-center">Stotelė nr.1</div>
+							<div class="card-body">stotelės aprašymas</div>
 							<input type="checkbox" name="igula_1_narys_1" style="margin: auto;"/>
 						</div>
 					</div>
 					<div class="col">
 						<div class="card">
-							<div class="card-title text-center ">Narys nr.2</div>
-							<div class="card-body">nario foto</div>
+							<div class="card-title text-center ">Stotelė nr.2</div>
+							<div class="card-body">stotelės aprašymas</div>
 							<input type="checkbox" name="igula_1_narys_2" style="margin: auto;"/>
 						</div>
 					</div>
 					<div class="col">
 						<div class="card">
-							<div class="card-title text-center">Narys nr.3</div>
-							<div class="card-body">nario foto</div>
+							<div class="card-title text-center">Stotelė nr.3</div>
+							<div class="card-body">stotelės aprašymas</div>
 							<input type="checkbox" name="igula_1_narys_3" style="margin: auto;"/>
 						</div>
 					</div>
 					<div class="col">
 						<div class="card">
-							<div class="card-title text-center">Narys nr.4</div>
-							<div class="card-body">nario foto</div>
+							<div class="card-title text-center">Stotelė nr.4</div>
+							<div class="card-body">stotelės aprašymas</div>
 							<input type="checkbox" name="igula_1_narys_4" style="margin: auto;"/>
 						</div>
 					</div>
