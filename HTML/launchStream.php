495c691ba8e4e51d82970c2965658dbebbd126ce
Vismantas <vabrom@gmail.com>
2019-12-18 23:20:58 +0200

    changed launch video

--- HTML/launchStream.php
+++ HTML/launchStream.php
@@ -7,7 +7,7 @@
 			<hr>
 			<p>Paspauskite ant žemiau esančio grotuvo norint pradėti tiesioginės transliacijos stebėjimą</p>
 			<div class="embed-responsive embed-responsive-16by9">
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/Wf5iBzg79EI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<video src="resources/launch.mp4" controls></video>
 			</div>
 		</div>
 
