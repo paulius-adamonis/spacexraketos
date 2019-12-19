e0cedda4b1b5387cf49db4d148bb25bd3d0448e3
Edvin Dičkanec <edvinas.dichkanec@gmail.com>
2019-12-20 01:23:49 +0200

    Rows in rocket creation class rearangement, carousel autoslide disabled

--- HTML/raketoskurimas.php
+++ HTML/raketoskurimas.php
@@ -16,53 +16,6 @@ require_once("includes/header.php");
             </div>
         </div>
         <hr>
-
-		<div class="d-flex justify-content-center">
-	        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-			  <div class="carousel-inner">
-			    <div class="carousel-item active">
-			      <img class="d-block w-100 h-100" id="image1" src="raketuFoto/Sviesi/Default/1.png" alt="Šiaurės kampas">
-			    </div>
-			    <div class="carousel-item">
-			      <img class="d-block w-100 h-100" id="image2" src="raketuFoto/Sviesi/Default/2.png" alt="Rytų kampas">
-			    </div>
-			    <div class="carousel-item">
-			      <img class="d-block w-100 h-100" id="image3" src="raketuFoto/Sviesi/Default/3.png" alt="Pietų kampas">
-			    </div>
-			    <div class="carousel-item">
-			      <img class="d-block w-100 h-100" id="image4" src="raketuFoto/Sviesi/Default/4.png" alt="Vakarų kampas">
-			    </div>
-			  </div>
-			  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			    <span class="sr-only">Atgal</span>
-			  </a>
-			  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-			    <span class="sr-only">Sekantis</span>
-			  </a>
-			</div>
-		</div>
-
-        <hr>
-        <div class="row">
-            <div class="col">
-            <h4 class="text-center">Raketos dizainas</h4>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col d-flex justify-content-center">
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="designRadio" id="inlineRadio1" value="option1" checked onclick="func();">
-                    <label class="form-check-label" for="inlineRadio1">Šviesi</label>
-                </div>
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="designRadio" id="inlineRadio2" value="option2" onclick="func();">
-                    <label class="form-check-label" for="inlineRadio2">Tamsi</label>
-                </div>
-            </div>
-        </div>
-        <hr>
         <div class="row">
             <div class="col">
             <h4 class="text-center">Kuro tipai</h4>
@@ -88,6 +41,53 @@ require_once("includes/header.php");
                 </div>
             </div>
         </div>
+        <hr>
+        <div class="row">
+            <div class="col">
+            <h4 class="text-center">Raketos dizainas</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col d-flex justify-content-center">
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="designRadio" id="inlineRadio1" value="option1" checked onclick="func();">
+                    <label class="form-check-label" for="inlineRadio1">Šviesi</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="designRadio" id="inlineRadio2" value="option2" onclick="func();">
+                    <label class="form-check-label" for="inlineRadio2">Tamsi</label>
+                </div>
+            </div>
+        </div>
+        <hr>
+
+		<div class="d-flex justify-content-center">
+	        <div id="carouselExampleControls" class="carousel" data-keyboard="true">
+			  <div class="carousel-inner">
+			    <div class="carousel-item active">
+			      <img class="d-block w-100 h-100" id="image1" src="raketuFoto/Sviesi/Default/1.png" alt="Šiaurės kampas">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100 h-100" id="image2" src="raketuFoto/Sviesi/Default/2.png" alt="Rytų kampas">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100 h-100" id="image3" src="raketuFoto/Sviesi/Default/3.png" alt="Pietų kampas">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100 h-100" id="image4" src="raketuFoto/Sviesi/Default/4.png" alt="Vakarų kampas">
+			    </div>
+			  </div>
+			  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Atgal</span>
+			  </a>
+			  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Sekantis</span>
+			  </a>
+			</div>
+		</div>
+
         <hr>
         <div class="d-flex justify-content-center">
         	<div class="row w-100 mb-3">
@@ -131,6 +131,7 @@ require_once("includes/header.php");
                 </div>
             </div>
         </div>
+
     </div>
     <br>
     <script>
