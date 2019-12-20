4f899bfb831960a6168dbefa26c56dee049bb82f
Paulius Adamonis <paulius.adamonis@ktu.edu>
2019-12-20 08:05:37 +0200

    Pridėjau logo ir sutvarkiau admin bugą

--- HTML/includes/header.php
+++ HTML/includes/header.php
@@ -10,10 +10,18 @@
 		<title>SpaceX Raketų Sistema</title>
 		<meta content="">
 	</head>
+	<style>
+		img.animated-gif{
+		width: 200px;
+		height: auto;
+		}
+	</style>
 	<body id="rect" >
 		<!-- Navigacija -->
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="#">SpaceX Raketų Sistema</a>
+			<a class="navbar-brand" href="#">
+				<img class="animated-gif" src="../resources/logo.gif" alt="" class="d-inline-block align-top">
+			</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
